abb2ba24561aac4c14ab1f2b3c00a5f4dac532e6
Mundia1 <waithaka.mundia@gmail.com>
2025-04-21 15:32:24 +0300

    update: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,26 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['react-router-dom']
+        }
+      }
+    }
   }
 })
