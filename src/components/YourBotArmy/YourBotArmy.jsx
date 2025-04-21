55efbe07bf2369c0e68ba7b8f9a7a97c74baf1ef
Mundia1 <waithaka.mundia@gmail.com>
2025-04-21 15:11:15 +0300

    feat: add src/components/YourBotArmy/YourBotArmy.jsx code

---
+++ src/components/YourBotArmy/YourBotArmy.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import BotCard from "../BotCard/BotCard";
+import styles from "./YourBotArmy.module.css";
+
+const YourBotArmy = ({ army, releaseBot, dischargeBot }) => {
+  return (
+    <div className={styles.armyContainer}>
+      <h2>Your Bot Army</h2>
+      <div className={styles.armyDescription}>
+        {army.length === 0 ? (
+          <p>You don't have any bots in your army yet. Click on bots to enlist them!</p>
+        ) : (
+          <p>Click on a bot to release it from your army.</p>
+        )}
+      </div>
+      <div className={styles.botArmy}>
+        {army.map((bot) => (
+          <BotCard 
+            key={bot.id} 
+            bot={bot} 
+            handleClick={releaseBot}
+            handleDischarge={dischargeBot}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default YourBotArmy;
